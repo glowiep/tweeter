ecae1870cd037d936f3f6303b46be29ab8155a06
Gloria Lim <glorialimartt@gmail.com>
2023-11-17 23:19:44 -0500

    Add function to hide scroll button when the vertical scroll position is at the top of page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,11 +8,24 @@ $(document).ready(function() {
     $("#compose-tweet").toggle(); // show or hide compose-tweet section
     $("#tweet-text").focus(); // Enable the textarea automatically
   });
+  
+  // Scroll button is hidden only when the vertical scroll position is 0
+  $("#scroll-button").hide();
+  $(window).on("scroll", function(event) {
+    event.preventDefault;
+    
+    let $scroll = $(window).scrollTop();
+    if ($scroll === 0) {
+      return $("#scroll-button").hide();
+    }
+    $("#scroll-button").show();
+  })
 
   // Scroll to top button function
-  $("#jump-to-top").on("click", function(event) {
+  $("#scroll-button").on("click", function(event) {
     event.preventDefault;
     $("html, body").animate({ scrollTop: 0 }, "slow");
+    return $("#compose-tweet").show().find("#tweet-text").focus();
   })
 
   //  Render tweets in tweets-container
